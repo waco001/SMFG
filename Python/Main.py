ca2578874aceb99d67df8c8e8052b94d6f65b7a6
waco001 <abhishek.gortigmail.com>
2015-07-13 17:01:37 -0400

    Added search, compare pages, NEXT: Need to sort out genes with spaces in between

--- Python/Main.py
+++ Python/Main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 app.config.update(
     DEBUG=True,
@@ -7,8 +7,15 @@ app.config.update(
 )
 
 @app.route("/")
-def hello():
+def index():
     return render_template('index.html')
 
+@app.route('/search')
+def search():
+	args = request.args
+	if(args.get('method') == 'compare'):
+		return render_template('compare.html', queries=args['q'])
+	else:
+		return render_template('search.html', queries=args['q'])
 if __name__ == "__main__":
     app.run()
